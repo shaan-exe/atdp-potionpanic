bae4ef062bc755ea6e69d6ee8cff98bb6e756643
Shaan Makim <makimshaan8@gmail.com>
2025-07-21 20:02:08 -0700

    ...

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: '/shaandabomb/projects/p2/assets/index-6x1meezR.js',
-        chunkFileNames: '/shaandabomb/projects/p2/assets/[name].js',
-        assetFileNames: '/shaandabomb/projects/p2/assets/[name][extname]',
+        entryFileNames: '/projects/p2/assets/index-6x1meezR.js',
+        chunkFileNames: '/projects/p2/assets/[name].js',
+        assetFileNames: '/projects/p2/assets/[name][extname]',
       }
     }
   }
